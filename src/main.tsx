c5ba92fc98d9b8f3cbf070146ec31d6fc405d3e9
Siddarth Kasuba <siddarthkasuba@gmail.com>
2026-07-18 14:24:55 +0000

    fix: safeguard canvas getBoundingClientRect polyfill for sandboxed iframe environments

--- src/main.tsx
+++ src/main.tsx
@@ -10,40 +10,48 @@ if (typeof window !== 'undefined') {
     const originalCreateElement = document.createElement;
     document.createElement = function(tagName: string, options?: ElementCreationOptions) {
       const el = originalCreateElement.call(document, tagName, options);
-      if (el && tagName.toLowerCase() === 'canvas' && !el.getBoundingClientRect) {
-        el.getBoundingClientRect = function() {
-          const width = (el as any).width || 0;
-          const height = (el as any).height || 0;
+      if (el && tagName.toLowerCase() === 'canvas' && typeof el.getBoundingClientRect !== 'function') {
+        Object.defineProperty(el, 'getBoundingClientRect', {
+          value: function() {
+            const width = (el as any).width || 0;
+            const height = (el as any).height || 0;
+            return {
+              x: 0,
+              y: 0,
+              top: 0,
+              left: 0,
+              bottom: height,
+              right: width,
+              width: width,
+              height: height,
+              toJSON: () => ""
+            };
+          },
+          configurable: true,
+          writable: true
+        });
+      }
+      return el;
+    } as typeof document.createElement;
+
+    if (typeof HTMLCanvasElement !== 'undefined' && typeof HTMLCanvasElement.prototype.getBoundingClientRect !== 'function') {
+      Object.defineProperty(HTMLCanvasElement.prototype, 'getBoundingClientRect', {
+        value: function() {
           return {
             x: 0,
             y: 0,
             top: 0,
             left: 0,
-            bottom: height,
-            right: width,
-            width: width,
-            height: height,
+            bottom: this.height || 0,
+            right: this.width || 0,
+            width: this.width || 0,
+            height: this.height || 0,
             toJSON: () => ""
           };
-        };
-      }
-      return el;
-    } as typeof document.createElement;
-
-    if (typeof HTMLCanvasElement !== 'undefined' && !HTMLCanvasElement.prototype.getBoundingClientRect) {
-      HTMLCanvasElement.prototype.getBoundingClientRect = function() {
-        return {
-          x: 0,
-          y: 0,
-          top: 0,
-          left: 0,
-          bottom: this.height || 0,
-          right: this.width || 0,
-          width: this.width || 0,
-          height: this.height || 0,
-          toJSON: () => ""
-        };
-      };
+        },
+        configurable: true,
+        writable: true
+      });
     }
   } catch (e) {
     console.warn("Canvas polyfill warning:", e);
